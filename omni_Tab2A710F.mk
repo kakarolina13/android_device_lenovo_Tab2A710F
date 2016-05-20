b4c1f7411c6fc3dc3837ff341b52f02b74e7d9ff
Seb <sbordenave@gmail.com>
2016-05-20 23:35:21 +0200

    modifié :         omni_Tab2A710F.mk

--- omni_Tab2A710F.mk
+++ omni_Tab2A710F.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Time Zone data Credits to desalesouche => http://forum.xda-developers.com/showpost.php?p=64850444&postcount=1204
 PRODUCT_COPY_FILES += \
+# Time Zone data Credits to desalesouche => http://forum.xda-developers.com/showpost.php?p=64850444&postcount=1204
     bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata \
     $(LOCAL_PATH)/kernel:kernel
 
