8da39154174f5a966116c4b6bffc8bb64bc56d99
Seb <sbordenave@gmail.com>
2016-05-20 23:30:33 +0200

    modifié :         BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,15 +1,15 @@
+# Device
 TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
 BOARD_MKBOOTIMG_ARGS := --base 80000000 --pagesize 2048 --kernel_offset 00008000 --ramdisk_offset 04000000 --tags_offset 00000100    --board 1438361675 
-BOARD_CUSTOM_BOOTIMG_MK := device/lenovo/Tab2A710F/boot.mk
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216              # = 16MB (OK with MTKDroidTools value)
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216          # = 16MB (OK with MTKDroidTools value)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736          # = 1,5 GB  (OK with MTKDroidTools value)
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472        # = 3 GB on 5,5 GB max  (OK with MTKDroidTools value)
-BOARD_CACHEIMAGE_PARTITION_SIZE := 132120576      # = 0x07e00000 = 128MB
+BOARD_CACHEIMAGE_PARTITION_SIZE := 132120576      	# = 0x07e00000 = 128MB
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
 
@@ -34,21 +34,24 @@ TW_INCLUDE_CRYPTO := true
 TW_EXTRA_LANGUAGES := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_NO_EXFAT_FUSE := true
 TW_INCLUDE_NTFS_3G := true
 FLASH_FROM_STORAGE := true # flashes zips from their current location instead of copying them to /tmp
+
+
+### retirés
+############
+#BOARD_CUSTOM_BOOTIMG_MK := device/lenovo/Tab2A710F/boot.mk
+#TW_NO_EXFAT_FUSE := true
+
 
 ### en test
 ###############
 
-
 ### A tester
 #############
-
 
 ### Testé, rejeté
 ##################
 #DEVICE_RESOLUTION := 1024x600	# Apparemment inutile
 #TW_NEVER_UNMOUNT_SYSTEM := true # Prevent TWRP from unmounting /system
-
 
