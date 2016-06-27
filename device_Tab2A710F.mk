89577b71af6000911303b8c0f5727816b7cb9111
pix106 <pix106@users.noreply.github.com>
2016-06-27 23:36:23 +0200

    Update device_Tab2A710F.mk

--- device_Tab2A710F.mk
+++ device_Tab2A710F.mk
@@ -21,4 +21,4 @@ $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_Tab2A710F
-PRODUCT_DEVICE := Tab2A7-10F
+PRODUCT_DEVICE := Tab2A710F
