ca14828e3a7bed651ccb852b66cdfaefa53d3176
Sébastien Bordenave <sbordenave@gmail.com>
2016-07-03 21:57:29 +0200

    Revert "added fstab, overlay, blobs..."
    This reverts commit bbaa5552a8d9285865eb76fe43eae3250cafb66c.
    
    	modifié :         BoardConfig.mk
    	supprimé :        TwrpConfig.mk
    	modifié :         device_Tab2A710F.mk
    	supprimé :        egl.cfg
    	supprimé :        extract-files.sh
    	supprimé :        extract-files_from_dump.sh
    	supprimé :        libxlog/Android.mk
    	supprimé :        libxlog/mtkaudio.cpp
    	supprimé :        libxlog/xlog.c
    	supprimé :        overlay/frameworks/base/core/res/res/xml/storage_list.xml
    	supprimé :        proprietary-files.txt
    	supprimé :        ramdisk/fstab
    	supprimé :        ramdisk/fstab.mt8127
    	supprimé :        ramdisk/init.mt8127.rc
    	supprimé :        ramdisk/init.mt8127usb.rc
    	supprimé :        ramdisk/system/bin/sh
    	supprimé :        ramdisk/ueventd.mt8127.rc
    	renommé :         recovery-twrp.fstab -> recovery.fstab
    	modifié :         system.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,17 +5,16 @@ USE_CAMERA_STUB := true
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := mt8127
+TARGET_BOARD_PLATFORM := unknown
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-ARCH_ARM_HAVE_NEON := true
+
 TARGET_BOOTLOADER_BOARD_NAME := Tab2A710F
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_USERIMAGES_USE_EXT4 := true
+
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216		# = 16MB (OK with MTKDroidTools value)
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216		# = 16MB (OK with MTKDroidTools value)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736		# = 1,5 GB  (OK with MTKDroidTools value)
@@ -23,39 +22,51 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472	# = 3 GB on 5,5 GB max  (OK wit
 BOARD_CACHEIMAGE_PARTITION_SIZE := 132120576		# = 0x07e00000 = 128MB
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file      # TEST
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
 
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_BOARD_PLATFORM_GPU := mali-450	# TEST
-
-
-# Kernel
-BOARD_KERNEL_CMDLINE :=
-BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := 
+#BOARD_KERNEL_CMDLINE := androidboot.selinux=disabled
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
+
 #TARGET_KERNEL_SOURCE := kernel/lenovo/Tab2A710F
 #TARGET_KERNEL_CONFIG := bitland8127_tb_l_defconfig
 TARGET_PREBUILT_KERNEL := device/lenovo/Tab2A710F/kernel
 
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+RECOVERY_VARIANT := twrp
+#
 # Debug
+#
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
-# Graphics
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/lenovo/Tab2A710F/egl.cfg
+#
+# TWRP
+#
+TWHAVE_SELINUX := true
+#TW_USE_TOOLBOX := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone1/temp"
+TW_MAX_BRIGHTNESS := 255		# Max brightness to prevent display damage
+TW_DEFAULT_BRIGHTNESS := 128
+TW_EXTRA_LANGUAGES := true
+#TW_NO_REBOOT_BOOTLOADER := true
 
+# Filesystem and storage
+TW_INCLUDE_CRYPTO := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+FLASH_FROM_STORAGE := true		# flashes zips from their current location instead of copying them to /tmp
+TW_INCLUDE_NTFS_3G := true
+#TW_NO_EXFAT_FUSE := true
+#TW_NO_EXFAT := true
+#TARGET_USERIMAGES_USE_F2FS := true	# Compatible kernel needed
+#TW_NEVER_UNMOUNT_SYSTEM := true	# Prevent TWRP from unmounting /system
 
+# Theme and graphics
+TW_THEME := portrait_mdpi
+#TW_CUSTOM_THEME := device/lenovo/Tab2A710F/recovery/twres
 
-# SELinux
-BOARD_SEPOLICY_DIRS := \
-       device/lenovo/Tab2A710F/sepolicy
-
-#BOARD_SEPOLICY_UNION := \
-#       app.te \
-#       system.te \
-
-
-# TWRP config
-#-include device/lenovo/Tab2A710F/TwrpConfig.mk
+USE_OPENGL_RENDERER    := true
