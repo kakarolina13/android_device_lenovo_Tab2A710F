bcb4d0bd1a10876934837d1255cde0cce2240026
Sébastien Bordenave <sbordenave@gmail.com>
2016-07-03 22:05:39 +0200

    added init scripts added adb to system.prop

--- device_Tab2A710F.mk
+++ device_Tab2A710F.mk
@@ -7,7 +7,6 @@ $(call inherit-product-if-exists, vendor/lenovo/Tab2A710F/Tab2A710F-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lenovo/Tab2A710F/overlay
 
-
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/lenovo/Tab2A710F/kernel
 else
@@ -22,3 +21,24 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_Tab2A710F
 PRODUCT_DEVICE := Tab2A710F
+
+
+TARGET_BUILD_VARIANT := eng
+TARGET_BUILD_TYPE := debug
+
+
+# sh	# Is there an other way of avoiding - exec '/system/bin/sh' failed: No such file or directory (2) - ?
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/system/bin/sh:root/system/bin/sh \
+
+# fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/fstab:root/fstab \
+    $(LOCAL_PATH)/ramdisk/fstab.mt8127:root/fstab.mt8127
+
+# init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/init.mt8127.rc:root/init.mt8127.rc \
+    $(LOCAL_PATH)/ramdisk/init.mt8127usb.rc:root/init.mt8127usb.rc
+
+
